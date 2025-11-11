1aafd51a20a7061ae438c43e84c886328486c835
Tnhi22 <nhib2205895@student.ctu.edu.vn>
2025-11-11 16:22:12 +0700

    Hien thi danh sach cac contact lay ve tu server

---
+++ src/components/ContactList.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul class="list-group">
+    <li
+      v-for="(contact, index) in contacts"
+      :key="index"
+      class="list-group-item"
+      :class="{ active: index === activeIndex }"
+      @click="updateActiveIndex(index)"
+    >
+      {{ contact.name }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    contacts: {
+      type: Array,
+      default: () => [],
+    },
+    activeIndex: {
+      type: Number,
+      default: -1,
+    },
+  },
+  emits: ["update:activeIndex"],
+  methods: {
+    updateActiveIndex(index) {
+      this.$emit("update:activeIndex", index);
+    },
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/js/all.min.js";
 
-createApp(App).mount('#app')
+import router from "./router";
+
+createApp(App).use(router).mount("#app");
